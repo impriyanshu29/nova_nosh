283a3fb8d667d3f6f86cef99aede7f6e016baf7b
iampriyanshu29 <impriyanshu2901@gmail.com>
2024-04-06 02:52:36 +0530

    Created USer Model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,53 @@
+import e from 'express';
 import mongoose from 'mongoose';
 import {Schema} from 'mongoose';
 
 const userSchema = new Schema({
-    
-})
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+       
+    },
+    phone:{
+        type: Number,
+       
+    },
+    image:{
+        type: String,
+       
+    },
+    refreshToken:{
+        type: String,
+       
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken:String,
+        forgotPasswordExpire:Date,
+        verifyToken:String,
+        verifyTokenExpire:Date,
+},{timestamps:true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
