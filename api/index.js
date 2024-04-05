e75bdae0180f1eed307e2228839801515eeb662a
iampriyanshu29 <impriyanshu2901@gmail.com>
2024-04-06 01:24:02 +0530

    Connected MongoDB

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import connectDB from './db/index.js';
+import app from './app.js';
+
+
+import dotenv from "dotenv";
+dotenv.config({
+    path:"./.env"
+});
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("Server failed to start ->", error);
+});
+
+
+
+
+//The listen method in Express.js is used to start a server and make it listen for incoming HTTP requests on a specific port.
